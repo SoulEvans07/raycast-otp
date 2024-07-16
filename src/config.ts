1e12247ee0d532c033a9d8d1674726baeb07196d
Adam Szi <szi.adam@simonyi.bme.hu>
2024-07-16 22:50:57 +0200

    feat: changed prio to index, migrate old data

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
-import { Color } from "@raycast/api";
+import { Color } from '@raycast/api';
 
 export const config = {
   colors: {
     key: Color.SecondaryText,
-    prio: {
+    index: {
       positive: Color.SecondaryText, // Color.Green,
       negative: Color.SecondaryText, // Color.Red,
     },
